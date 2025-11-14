379dfe0f6745ecae7cd304e253dc83a2edb0d597
abdulll_ <150576301+abdull102@users.noreply.github.com>
2025-11-14 19:48:55 +0700

    Add files via upload

---
+++ public/icons/Menu.jsx
@@ -0,0 +1,17 @@
+export default function MenuIcon({ className }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      className={className}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeWidth={1.5}
+        d="M20 7H4m16 5H4m16 5H4"
+      ></path>
+    </svg>
+  );
+}
